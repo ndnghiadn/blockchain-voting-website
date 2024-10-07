b9f42ee94f0b8c8f038839ad0173c7f172be8e25
Nguyễn Đình Nghĩa <102190027@sv1.dut.udn.vn>
2024-10-07 23:22:57 +0700

    feat: update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import LoginForm from "@/components/LoginForm";
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
-  return <LoginForm />;
+  return <HomePage />;
 }
